55c240ffa04d322a7719b191de71be303f4b8e0b
久居你心 <712042812@qq.com>
2017-09-06 00:06:03 +0800

    二次修改

--- src/Top.php
+++ src/Top.php
@@ -1,65 +1,42 @@
 <?php
-/**
- * TOP SDK 入口文件
- * 请不要修改这个文件，除非你知道怎样修改以及怎样恢复
- * @author xuteng.xt
- */
-
-/**
- * 定义常量开始
- * 在include("TopSdk.php")之前定义这些常量，不要直接修改本文件，以利于升级覆盖
- */
-/**
- * SDK工作目录
- * 存放日志，TOP缓存数据
- */
-if (!defined("TOP_SDK_WORK_DIR"))
-{
-    define("TOP_SDK_WORK_DIR", "/tmp/");
-}
-
-/**
- * 是否处于开发模式
- * 在你自己电脑上开发程序的时候千万不要设为false，以免缓存造成你的代码修改了不生效
- * 部署到生产环境正式运营后，如果性能压力大，可以把此常量设定为false，能提高运行速度（对应的代价就是你下次升级程序时要清一下缓存）
- */
-if (!defined("TOP_SDK_DEV_MODE"))
-{
-    define("TOP_SDK_DEV_MODE", true);
-}
-
-if (!defined("TOP_AUTOLOADER_PATH"))
-{
-    define("TOP_AUTOLOADER_PATH", dirname(__FILE__));
-}
-
-/**
-* 注册autoLoader,此注册autoLoader只加载top文件
-* 不要删除，除非你自己加载文件。
-**/
 namespace taobao;
 use Yii;
 use yii\base\Component;
-class Top extends Component{
+class Top extends Component
+{
+	private static $classMap;
 
-    //挂载树
-    private static $app;
+	public function __construct()
+	{
+		if (!defined("TOP_SDK_WORK_DIR"))
+		{
+			define("TOP_SDK_WORK_DIR", "/tmp/");
+		}
 
-    public function __get($param)
-    {
-        
-        if(!isset(static::$app[$param])){
-            //自动加载
-            spl_autoload_register('Top::autoload');
-            //实例化对象并保存
-            static::$app[$param] = new $param; 
-        }
+		if (!defined("TOP_SDK_DEV_MODE"))
+		{
+			define("TOP_SDK_DEV_MODE", true);
+		}
 
-        return static::$app[$param];
-    }
-  
+		if (!defined("TOP_AUTOLOADER_PATH"))
+		{
+			define("TOP_AUTOLOADER_PATH", dirname(__FILE__));
+		}
 
-   /**
+		spl_autoload_register('Top::autoload');
+	}
+
+	//因为这是第三方代码，为了完全使用第三方结构代码，所以重构了__get方法
+	public function __get($name)
+	{
+		if(!isset(self::$classMap[$name])){
+			self::$classMap[$name] = new $name;
+		}
+
+		return self::$classMap;
+	}
+
+  	/**
      * 类库自动加载，写死路径，确保不加载其他文件。
      * @param string $class 对象类名
      * @return void
@@ -107,4 +84,4 @@ class Top extends Component{
         }         
     }
 }
-?>
+
